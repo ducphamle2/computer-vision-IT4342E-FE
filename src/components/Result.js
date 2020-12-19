e10b499d7a61596261366a94e906c35000556170
Le Duc Pham <ducphamle212@gmail.com>
2020-12-19 23:39:43 +0700

    fix: fix result render

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,30 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // import download from 'downloadjs';
 // import axios from 'axios';
 // import { API_URL } from '../utils/constants';
 import { useLocation } from 'react-router-dom';
 
 var listOfImages = []
-const importAll = (r) => {
-  return r.keys().map(r);
-}
-listOfImages = importAll(require.context('/mnt/d/Git repo/computer-vision-IT4342E/ocr_manga/executables/tmp_images/inpainted', false, /\.(png|jpe?g)$/));
 
 const Result = () => {
-  
+
   const location = useLocation();
+
+  const importAll = (r) => {
+    return r.keys().map(r);
+  }
+  const [counter, setCounter] = useState(0);
   // const [Result, setResult] = useState([]);
   // const [errorMsg, setErrorMsg] = useState('');
   useEffect(() => {
+    listOfImages = importAll(require.context('../../../computer-vision-IT4342E/ocr_manga/executables/tmp_images/translated/', false, /\.(png|jpe?g)$/));
     console.log(listOfImages)
-    console.log("location after passing from main component: ", location.state)
+    setCounter(1)
   }, [])
 
   return (
     <div>
       {
         listOfImages.map(
-          (index, image) => <img key={index} src={image} alt="info"></img>
+          (image, index) => <img key={index} src={image} alt="info"></img>
         )
       }
     </div>
